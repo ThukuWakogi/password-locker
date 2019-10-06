48816dad6b974d9a5b2e456ac102713505d743d4
Timothy Oliver <oliverwakogi@live.com>
2019-10-06 20:49:33 +0300

    create intro text for the app

---
+++ password-locker.py
@@ -0,0 +1,10 @@
+# lays out welcome text
+print('-' * 100)
+print('\t' * 5)
+print(' ' * 42 + 'PASSWORD LOCKER')
+print('\t' * 3)
+print(' ' * 20 + 'Manage your passwords across multiple social media platforms')
+print('\t' * 5)
+print('-' * 100)
+
+
