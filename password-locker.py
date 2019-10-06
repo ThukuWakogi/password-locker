d794ce162f9a270aafdb059be74ee6d3daeeb603
Timothy Oliver <oliverwakogi@live.com>
2019-10-06 21:08:48 +0300

    Add method to check if data file exists

--- password-locker.py
+++ password-locker.py
@@ -1,3 +1,18 @@
+def check_datafile_exists():
+  '''
+  checks if data file exists
+
+  returns:
+    True: if data file exists
+    False: if data file doesn't exist
+  '''
+  try:
+    file = open('data.txt')
+    file.close()
+    return True
+  except FileNotFoundError:
+    return False
+
 # lays out welcome text
 print('-' * 100)
 print('\t' * 5)
@@ -6,5 +21,3 @@ print('\t' * 3)
 print(' ' * 20 + 'Manage your passwords across multiple social media platforms')
 print('\t' * 5)
 print('-' * 100)
-
-
