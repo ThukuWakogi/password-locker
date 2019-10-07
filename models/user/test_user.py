6ea963a89c2d94c9f8840537549387f55b46e2d6
Timothy Oliver <oliverwakogi@live.com>
2019-10-07 14:04:16 +0300

    add user initialization  test

--- models/user/test_user.py
+++ models/user/test_user.py
@@ -1,4 +1,25 @@
 import unittest
+from user import User
 
 class TestUser(unittest.TestCase):
-  pass
+  '''
+  tests if all user functionality works
+  '''
+
+  def setUp(self):
+    '''
+    set up method to run before each use case
+    '''
+
+    self.user = User(username='wayne', password='rockfeller')
+
+  def test_init(self):
+    '''
+    tests if the object initializes
+    '''
+    
+    self.assertEqual(self.user.username, 'wayne')
+    self.assertEqual(self.user.password, 'rockfeller')
+
+if __name__ == '__main__':
+  unittest.main()
